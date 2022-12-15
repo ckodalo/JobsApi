e2ae5eec214433b05c58bda2cb4957def5d9d2ec
calvin <ckodalo@gmail.com>
2022-12-15 19:08:14 +0300

    added cookie configurations

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,13 @@ module JobsApi
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
+       # Must add these lines!
+    # Adding back cookies and session middleware
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+
+    # Use SameSite=Strict for all cookies to help protect against CSRF
+    config.action_dispatch.cookies_same_site_protection = :strict
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
   end
