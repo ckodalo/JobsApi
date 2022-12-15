183972d2c660881fd000b4fe7f0638b9d6664477
calvin <ckodalo@gmail.com>
2022-12-15 19:29:49 +0300

    added auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   resources :users
   resources :jobs
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
