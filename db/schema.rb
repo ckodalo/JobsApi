e82b0ba265b9bd10a78f54c07838d51c95866b97
calvin <ckodalo@gmail.com>
2022-12-15 19:23:29 +0300

    added bcrypt

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_15_154249) do
-  create_table "jobs", force: :cascade do |t|
-    t.string "title"
-    t.string "recruiter"
-    t.string "ifremote"
-    t.string "location"
-    t.integer "user_id"
-    t.string "stack"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.0].define(version: 0) do
 end
