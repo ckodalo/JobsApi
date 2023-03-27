1d2d6fa0f4a2d7f65d068188b65998c9af03b5e2
calvin <ckodalo@gmail.com>
2023-03-27 19:33:19 +0300

    fix typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
         else
             render json: { message: 'Failed to create user account', errors: user.errors.full_messages }, status: :unprocessable_entity
         end
-        end
+    
     end
 
    
@@ -34,15 +34,15 @@ class UsersController < ApplicationController
 
 
 
-    # def show
+     def show
         
-    #     user = User.find_by(id: session[:user_id])
-    #     if user
-    #         render json: user.jobs
-    #     else
-    #         render json: { error: "Not authorized" }, status: :unauthorized
-    #     end
-    # end
+         user = User.find_by(id: session[:user_id])
+         if user
+             render json: user.jobs
+         else
+             render json: { error: "Not authorized" }, status: :unauthorized
+         end
+     end
 
 
     
