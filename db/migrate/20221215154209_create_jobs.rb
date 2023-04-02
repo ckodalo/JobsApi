df4fcb304306706372373c7c55de582e6be650cf
calvin <ckodalo@gmail.com>
2023-04-02 17:57:10 +0300

    EDIT: added image to jobs

--- db/migrate/20221215154209_create_jobs.rb
+++ db/migrate/20221215154209_create_jobs.rb
@@ -3,7 +3,6 @@ class CreateJobs < ActiveRecord::Migration[7.0]
     create_table :jobs do |t|
       t.string :title
       t.string :location
-      t.string :image
       t.string :recruiter
       t.string :details
       t.string :category
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_15_154249) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_02_145501) do
   create_table "jobs", force: :cascade do |t|
     t.string "title"
     t.string "location"
-    t.string "image"
     t.string "recruiter"
     t.string "details"
     t.string "category"
@@ -22,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_15_154249) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
