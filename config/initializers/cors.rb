ad63768aa2142b13065c535826b6a04a724ab3fc
calvin <ckodalo@gmail.com>
2023-05-17 10:54:28 +0300

    edit

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,18 +4,26 @@
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
 # Read more: https://github.com/cyu/rack-cors
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins '*'
+#     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+#   end
+# end
 
-   Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  #   allow do
-  #     origins "http://localhost:4000"
-  #     resource "*",
-  #       headers: :any,
-  #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  #   end
+
+#    Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#     allow do
+#       origins "http://localhost:4000"
+#       resource "*",
+#         headers: :any,
+#         methods: [:get, :post, :put, :patch, :delete, :options, :head]
+#     end
+#   end
   
-    # Allow requests from production environment
+    Allow requests from production environment
     allow do
-        origins "https://mtandao.xyz"
+        origins ".mtandao.xyz"
         resource '*', headers: :any, 
         methods: [:get, :post, :put, :patch, :delete, :options, :head]
       end
