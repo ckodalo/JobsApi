8d1b8d80abc2bce207f3dac994e48bdc83c9e6c3
calvin <ckodalo@gmail.com>
2023-05-17 17:06:25 +0300

    edits

--- config/application.rb
+++ config/application.rb
@@ -37,9 +37,9 @@ module JobsApi
        # Must add these lines!
     # Adding back cookies and session middleware
     config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore
+    # config.middleware.use ActionDispatch::Session::CookieStore
 
-    Rails.application.config.session_store :cookie_store, key: '_my_app_session', domain: :all, tld_length: 2
+    # Rails.application.config.session_store :cookie_store, key: '_my_app_session', domain: :all, tld_length: 2
 
 
     
