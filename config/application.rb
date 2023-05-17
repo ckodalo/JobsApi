c934df117f7c8d3324db6865d1f92161909490e1
calvin <ckodalo@gmail.com>
2023-05-17 11:49:15 +0300

    edit

--- config/application.rb
+++ config/application.rb
@@ -51,12 +51,12 @@ module JobsApi
     #   end
     # end
 
-    # Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    #   allow do
-    #     origins 'https://mtandao.xyz'
-    #     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
-    #   end
-    # end
+    Rails.application.config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'https://mtandao.xyz'
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
     
   
 
