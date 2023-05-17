14b3bc580862ed672f9859fff261e58cf19a7309
calvin <ckodalo@gmail.com>
2023-05-17 17:24:17 +0300

    edit

--- config/application.rb
+++ config/application.rb
@@ -39,7 +39,7 @@ module JobsApi
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
 
-    Rails.application.config.session_store :cookie_store, key: '_my_app_session', domain: :all, tld_length: 2
+    # Rails.application.config.session_store :cookie_store, key: '_my_app_session', domain: :all, tld_length: 2
 
 
     
