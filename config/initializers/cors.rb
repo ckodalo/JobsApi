948b21a43bc083dd17485c0f38ce6215f7d7228b
calvin <ckodalo@gmail.com>
2023-05-17 11:19:25 +0300

    edit

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,7 +13,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins ".mtandao.xyz"
+      origins 'https://mtandao.xyz'
       resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
     end
   end
