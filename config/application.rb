6627afc68cb10aabe6187671b84f386203320e55
calvin <ckodalo@gmail.com>
2023-05-18 09:20:42 +0300

    edit

--- config/application.rb
+++ config/application.rb
@@ -36,8 +36,8 @@ module JobsApi
     # Middleware like session, flash, cookies can be added back manually.
        # Must add these lines!
     # Adding back cookies and session middleware
-    config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore
+    # config.middleware.use ActionDispatch::Cookies
+    # config.middleware.use ActionDispatch::Session::CookieStore
 
     # Rails.application.config.session_store :cookie_store, key: '_my_app_session', domain: :all, tld_length: 2
 
